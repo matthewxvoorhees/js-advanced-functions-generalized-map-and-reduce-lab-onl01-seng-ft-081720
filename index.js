3a3a5804b93359572a389347dce5f972898ba486
matthewxvoorhees <matthew.crnich@gmail.com>
2020-12-02 17:29:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+  function map (arr, func){
+    return arr.map(x => func(x))
+}
+
+function reduce(arr, func, start = 0){
+    let x = [start, ...arr].reduce((x,y) => func(x,y))
+    if (x){
+        return x
+    } else if (arr.every(y => y)) {
+        return true
+    } else {
+        return false
+    }
+}
